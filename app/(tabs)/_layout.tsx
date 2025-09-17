7561c670cf8cdb8efe759781de91316f9a1d5f26
Chirag Rane <beingchirag7@gmail.com>
2025-09-17 16:27:40 +0530

    Tabbar setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,37 @@
-// app/(tabs)/_layout.tsx
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="Home" />
-      {/* Add more tab screens later */}
+    <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: 'blue' }}>
+
+      <Tabs.Screen name="Home" options={{
+        title: 'Home',
+        tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />
+      }} />
+
+      <Tabs.Screen name="Menu" options={{
+        title: 'Menu',
+        tabBarIcon: ({ color }) => <Ionicons size={28} name="fast-food" color={color} />
+      }} />
+
+      <Tabs.Screen name="Cart" options={{
+        title: 'Cart',
+        tabBarIcon: ({ color }) => <FontAwesome size={28} name="shopping-cart" color={color} />
+      }} />
+
+      <Tabs.Screen name="Orders" options={{
+        title: 'Orders',
+        tabBarIcon: ({ color }) => <FontAwesome5 size={28} name="file-invoice" color={color} />
+      }} />
+
+      <Tabs.Screen name="Profile" options={{
+        title: 'Profile',
+        tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />
+      }} />
+
     </Tabs>
   );
 }
